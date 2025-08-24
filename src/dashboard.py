82fc9077555162d17affd3577b9e6e847005a71d
Saloni Chauhan <schauhan1@hawk.iit.edu>
2025-08-23 18:16:39 -0600

    aug23-3

--- src/dashboard.py
+++ src/dashboard.py
@@ -62,7 +62,7 @@ st.markdown(
     }
     /* Target the Add to Plan button by its key */
     button[data-testid="stButton"]#add_to_plan_button {
-        background-color: #013787 !important;
+        background-color: #002769 !important; /* Darkest color */
         color: white !important;
         padding: 5px 15px !important;
         border-radius: 5px !important;
@@ -75,7 +75,7 @@ st.markdown(
     }
     /* Target the Generate Insight button by its key with margin adjustment */
     button[data-testid="stButton"]#generate_insight_button {
-        background-color: #013787 !important;
+        background-color: #002769 !important; /* Darkest color */
         color: white !important;
         padding: 5px 15px !important;
         border-radius: 5px !important;
